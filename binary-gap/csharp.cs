d3599ac947c92c321a59ab67d4b701cb19ee2bf0
Pablo Christian Nazareth <snakepablo@hotmail.com>
2023-12-04 12:32:43 -0300

    Create csharp.cs

---
+++ binary-gap/csharp.cs
@@ -0,0 +1,38 @@
+// See https://aka.ms/new-console-template for more information
+using System.Xml.Linq;
+
+Console.WriteLine("Hello, World!");
+
+var solution = new Solution();
+
+Console.WriteLine($"Max length is: {solution.Binary(529)}");
+
+Console.ReadKey();
+
+public class Solution
+{
+    public int Binary(int N)
+    {
+        int length = 0;
+        int max = 0;
+        string numbers = Convert.ToString(N, 2);
+        bool foundOne = false;
+
+        for(int i=0;i<numbers.Length; i++)
+        {
+            if (numbers[i].ToString().Equals("0") && foundOne)
+            {
+                length++;
+                if (length > max)
+                    max = length;
+            }
+            else
+            {
+                foundOne = true;
+                length = 0;
+            }
+        }
+
+        return max;
+    }
+}
